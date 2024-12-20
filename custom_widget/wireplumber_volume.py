b69631c3e1a8e16908e0a6937d52521bb6b469c8
Artemiy Shushuev <statiz.arm@gmail.com>
2024-12-21 00:05:54 +0300

    feat(volume): add next_sink command

--- custom_widget/wireplumber_volume.py
+++ custom_widget/wireplumber_volume.py
@@ -1,4 +1,5 @@
 import subprocess
+import itertools
 import enum
 
 from typing import Optional, Tuple, List
@@ -23,6 +24,53 @@ class WirePlumberVolumeSign(enum.Enum):
 
     def __str__(self):
         return self.value
+
+
+class WirePlumberSink:
+    def __init__(
+        self,
+        object_id: int,
+        nick: str,
+        is_default: Optional[bool]=False,
+    ):
+        self.object_id = object_id
+        self.nick = nick
+        self.is_default = is_default
+
+    @staticmethod
+    def _parse_sink_status_line(status_line: str):
+        tokens = status_line.split()
+        is_default = '*' in tokens
+        idx = 1
+        if is_default:
+            idx +=1
+        object_id = int(tokens[idx].strip('.'))
+        nick = ' '.join(
+            itertools.takewhile(
+                lambda x: not x.startswith('['), tokens[idx+1:]
+            )
+        )
+        return WirePlumberSink(
+            object_id=object_id,
+            nick=nick,
+            is_default=is_default,
+        )
+
+    @staticmethod
+    def parse_status_output(status_output: str):
+        lines = status_output.split('\n')
+        for idx, line in enumerate(lines):
+            if 'Sinks:' in line:
+                break
+        lines = lines[idx+1:]
+        for idx, line in enumerate(lines):
+            if 'Sources:' in line:
+                break
+        lines = lines[:idx-1]
+        return [
+            WirePlumberSink._parse_sink_status_line(line)
+            for line in lines
+        ]
 
 
 class WirePlumberConnection:
@@ -92,9 +140,17 @@ class WirePlumberConnection:
     def set_default(self, sink: int):
         cmd = [
             'set-default',
-            f'{sink if sink is not None else "@DEFAULT_AUDIO_SINK@"}',
+            f'{sink}',
         ]
         self._call_wpctl(cmd)
+
+    def list_sinks(self) -> List[WirePlumberSink]:
+        cmd = [
+            'status',
+            '--nick',
+        ]
+        out = self._call_wpctl(cmd)
+        return WirePlumberSink.parse_status_output(out)
 
 
 class WirePlumberVolume(Volume):
@@ -112,7 +168,14 @@ class WirePlumberVolume(Volume):
             }
         )
         self._connection = WirePlumberConnection()
-
+        self.add_callbacks(
+            {
+                "Button1": self.mute,
+                "Button2": self.next_sink,
+                "Button4": self.increase_vol,
+                "Button5": self.decrease_vol,
+            }
+        )
     def _configure(self, qtile, bar):
         Volume._configure(self, qtile, bar)
         if self.theme_path:
@@ -140,6 +203,21 @@ class WirePlumberVolume(Volume):
             value=value or self.step,
             sign=WirePlumberVolumeSign.DECREASE,
         )
+
+    @expose_command()
+    def next_sink(self):
+        """Set next sink as default"""
+        sinks = self._connection.list_sinks()
+        idx = 0
+        for idx in range(len(sinks)):
+            sink = sinks[idx]
+            if sink.is_default:
+                break
+
+        if idx < len(sinks):
+            self._connection.set_default(
+                sinks[(idx + 1) % len(sinks)].object_id
+            )
 
     def get_volume(self):
         return self._connection.get_volume()
